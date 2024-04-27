4ef1626c00ee96d109fba7ac75dc04d7c1580e5f
thulasiraman9468 <83747993+thulasiraman9468@users.noreply.github.com>
2024-04-27 17:29:54 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+  type = string
+  default = "us-east-1"
+}
+
+variable "access_key" {
+    type = string
+  default = "access_key"
+}
+
+variable "secret_key" {
+  type = string
+  default = "secret_key"
+}
